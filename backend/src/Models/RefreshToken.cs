91ec0164b759f02579c20ab4efc4992d5f6a283f
John Doe <nghuthjo@gmail.com>
2026-01-19 15:45:23 +0100

    feat(backend model): add revoked bool to refresh token model

--- backend/src/Models/RefreshToken.cs
+++ backend/src/Models/RefreshToken.cs
@@ -5,6 +5,7 @@ public class RefreshToken
     public int Id { get; set; }
     public required string Token { get; set; }
     public DateTime ExpiresAtUtc { get; set; }
+    public bool IsRevoked { get; set; } = false;
     public int UserId { get; set; }
     public User User { get; set; } = null!;
 }
