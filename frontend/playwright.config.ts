e8d15818429bd303d5f01ad90d83dbb74a470d33
John Doe <nghuthjo@gmail.com>
2026-05-28 20:01:18 +0200

    ops(frontend): add frontend tests to github workflow

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "https://localhost:5173",
+    baseURL: "http://localhost:5173",
     ignoreHTTPSErrors: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -92,7 +92,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev-test",
-    url: "https://localhost:5173",
+    url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
     ignoreHTTPSErrors: true,
   },
